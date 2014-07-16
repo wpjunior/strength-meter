e909a9ef2fdee28b143211d9856d2678b8ba07b9
Wilson Júnior <wilsonpjunior@gmail.com>
2014-07-16 11:46:15 -0300

    updated

--- js/strength-meter.js
+++ js/strength-meter.js
@@ -16,10 +16,10 @@
 (function ($) {
     "use strict";
 
-    String.prototype.reverseString = function () {
+    var reverseString = function (str) {
         var newstring = "";
-        for (var s = 0; s < this.length; s++) {
-            newstring = this.charAt(s) + newstring;
+        for (var s = 0; s < str.length; s++) {
+            newstring = str.charAt(s) + newstring;
         }
         return newstring;
     };
@@ -143,7 +143,7 @@
             /* Check for sequential alpha string patterns (forward and reverse) */
             for (var s = 0; s < 23; s++) {
                 var sFwd = sAlphas.substring(s, parseInt(s + 3));
-                var sRev = sFwd.reverseString();
+                var sRev = reverseString(sFwd);
                 if (text.toLowerCase().indexOf(sFwd) != -1 || text.toLowerCase().indexOf(sRev) != -1) {
                     nSeqAlpha++;
                     nSeqChar++;
@@ -153,7 +153,7 @@
             /* Check for sequential numeric string patterns (forward and reverse) */
             for (var s = 0; s < 8; s++) {
                 var sFwd = sNumerics.substring(s, parseInt(s + 3));
-                var sRev = sFwd.reverseString();
+                var sRev = reverseString(sFwd);
                 if (text.toLowerCase().indexOf(sFwd) != -1 || text.toLowerCase().indexOf(sRev) != -1) {
                     nSeqNumber++;
                     nSeqChar++;
@@ -163,7 +163,7 @@
             /* Check for sequential symbol string patterns (forward and reverse) */
             for (var s = 0; s < 8; s++) {
                 var sFwd = sSymbols.substring(s, parseInt(s + 3));
-                var sRev = sFwd.reverseString();
+                var sRev = reverseString(sFwd);
                 if (text.toLowerCase().indexOf(sFwd) != -1 || text.toLowerCase().indexOf(sRev) != -1) {
                     nSeqSymbol++;
                     nSeqChar++;
