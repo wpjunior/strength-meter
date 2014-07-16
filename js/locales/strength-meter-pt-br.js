ce8d07950b7cd5733451b2ed5c75d8bbcc76c1b8
Paolla <paolla.ocoutinho@gmail.com>
2014-07-16 12:01:15 -0300

    add translation PT-BR

---
+++ js/locales/strength-meter-pt-br.js
@@ -0,0 +1,20 @@
+/**
+ * Strength Meter German translation.
+ * This file must be saved in UTF-8 encoding.
+ * @copyright &copy; Kartik Visweswaran, Krajee.com, 2013
+ */
+(function ($) {
+    "use strict";
+
+    $.extend($.fn.strength.defaults, {
+        toggleTitle: 'Mostrar senha',
+        verdictTitles: {
+            0: 'Campo vazio',
+            1: 'Muito fraca',
+            2: 'Fraca',
+            3: 'Regular', 
+            4: 'Boa',
+            5: 'Excelente'
+        }
+    });
+})(jQuery);
